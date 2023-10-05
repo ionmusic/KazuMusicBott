3908356923397fe7c8e392f2ca42b66f89021fba
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-10-06 03:45:40 +0700

    update Clients.py

--- KazuMusic/Helpers/Clients.py
+++ KazuMusic/Helpers/Clients.py
@@ -9,4 +9,4 @@ app = Client(
     config.API_HASH,
     bot_token=config.BOT_TOKEN,
 )
-Ass = Client(config.STRING_SESSION, config.API_ID, config.API_HASH)
+Ass = Client("Anjay", config.STRING_SESSION, config.API_ID, config.API_HASH)
