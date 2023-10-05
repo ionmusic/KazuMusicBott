7d75779add3e9fc466735c3a0683315538812ab9
IONBOT <113283570+ionmusic@users.noreply.github.com>
2023-10-06 05:36:09 +0700

    Update permission.py

--- KazuMusic/Cache/permission.py
+++ KazuMusic/Cache/permission.py
@@ -6,7 +6,7 @@ from KazuMusic import BOT_ID, app
 def PermissionCheck(mystic):
     async def wrapper(_, message):
         a = await app.get_chat_member(message.chat.id, BOT_ID)
-        if a.status != "administrator":
+        if not a.status != "administrator":
             return await message.reply_text(
                 "» ᴩʟᴇᴀsᴇ ɢɪᴠᴇ ᴍᴇ ʙᴇʟᴏᴡ ᴩᴇʀᴍɪssɪᴏɴs :\n\n"
                 + "\n• **ᴅᴇʟᴇᴛᴇ ᴍᴇssᴀɢᴇs**"
